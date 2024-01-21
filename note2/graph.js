dcbadc07675e4e7eed35d2f6ec3d7bd7f3888cde
Ju-MINJAE <juminjae98@gmail.com>
2024-01-21 19:51:32 +0900

    Update graph.js

--- note2/graph.js
+++ note2/graph.js
@@ -28,17 +28,95 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  // DFS(깊이우선탐색) : 다른 형제를 방문하기 전에 한 브랜치에서 가능한 가장 아래까지 깊이 탐색
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
+
+  breadthFirst(start) {
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+    visited[start] = true;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
+
 var g = new Graph();
-g.addVertex('Seoul');
-g.addVertex('Busan');
-g.addVertex('Jeju');
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
 
-g.addEdge('Seoul', 'Busan');
-g.addEdge('Seoul', 'Jeju');
+g.addEdge('A', 'B');
+g.addEdge('A', 'C');
+g.addEdge('B', 'D');
+g.addEdge('C', 'E');
+g.addEdge('D', 'E');
+g.addEdge('D', 'F');
+g.addEdge('E', 'F');
 
-g.removeEdge('Seoul', 'Busan');
-
-g.removeVertex('Jeju');
-console.log(g);
+console.log(g.depthFirstRecursive('A')); //[ 'A', 'B', 'D', 'E', 'C', 'F' ]
+console.log(g.depthFirstIterative('A')); //[ 'A', 'C', 'E', 'F', 'D', 'B' ]
+console.log(g.breadthFirst('A')); //[ 'A', 'B', 'C', 'D', 'E', 'F' ]
