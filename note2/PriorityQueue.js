713c6c839703c7136e08ae957731109b9d35c9ae
Ju-MINJAE <juminjae98@gmail.com>
2024-01-22 18:04:53 +0900

    Update PriorityQueue.js

--- note2/PriorityQueue.js
+++ note2/PriorityQueue.js
@@ -2,13 +2,16 @@ class PriorityQueue {
   constructor() {
     this.values = [];
   }
+
   enqueue(val, priority) {
     this.values.push({ val, priority });
     this.sort();
   }
+
   dequeue() {
     return this.values.shift();
   }
+
   sort() {
     this.values.sort((a, b) => a.priority - b.priority);
   }
@@ -18,13 +21,16 @@ class WeightedGraph {
   constructor() {
     this.adjacencyList = {};
   }
+
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
+
   addEdge(vertex1, vertex2, weight) {
     this.adjacencyList[vertex1].push({ node: vertex2, weight });
     this.adjacencyList[vertex2].push({ node: vertex1, weight });
   }
+
   Dijkstra(start, finish) {
     const nodes = new PriorityQueue();
     const distances = {};
