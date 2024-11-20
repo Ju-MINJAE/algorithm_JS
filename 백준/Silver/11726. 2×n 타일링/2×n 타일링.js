d653d62afa33d3d71374c9b23d8e4ee177fda017
MinJae <juminjae98@gmail.com>
2024-11-20 15:56:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 156 ms, Memory: 9648 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim();
+
+let dp = [0, 1, 2];
+
+for (let i = 3; i <= Number(input); i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+console.log(dp[input]);
