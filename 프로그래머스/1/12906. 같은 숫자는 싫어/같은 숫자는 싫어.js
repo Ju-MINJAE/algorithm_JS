01e4ccf354bd08ae370ccb63e000ed4f5f4a1460
MinJae <juminjae98@gmail.com>
2025-03-21 14:15:18 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 14.65 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr)
+{
+    let answer = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (answer.length === 0 || arr[i] !== answer[answer.length - 1]){
+            answer.push(arr[i]);
+        }
+    }
+    
+    return answer;
+}
