73c7bd0fc9876a0b890d797a4174babecf2e9f30
MinJae <juminjae98@gmail.com>
2026-03-31 23:52:56 +0900

    [level 2] Title: 주식가격, Time: 28.69 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,14 @@
+
+function solution(prices) {
+    const n = prices.length;
+    const answer = new Array(n).fill(0);
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            answer[i]++;
+            if (prices[i] > prices[j]) break;
+        }
+    }
+    
+    return answer;
+}
